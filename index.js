b68c2ecc14f6a64531ec44e3fa25ec20b8b5eb66
Master1245 <62082195+Master1245@users.noreply.github.com>
2021-04-09 18:14:55 -0300

    inicial

---
+++ index.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+
+const bot = new Discord.Client();
+
+const token =  'ODI5Nzk5Mjk0MDIxNTMzNzM3.YG9Y0A.aaIQtOe3gQKqHyHO2MQhpaf1hrk'
+
+bot.login(token)
+
+bot.on('ready', () =>{
+    console.log('MAICK VIADO')
+})
+
+let gala = 0;
+bot.on('message', msg => {
+    
+    if(msg.content === "GALA" || msg.content === 'gala') {
+         gala = gala + 1;
+        msg.reply(gala)
+    }
+
+})
+
+let f = 0;
+bot.on('message', msg => {
+    
+    if(msg.content === "F" || msg.content === 'f') {
+        f = f + 1;
+        msg.reply(f)
+    }
+
+})
